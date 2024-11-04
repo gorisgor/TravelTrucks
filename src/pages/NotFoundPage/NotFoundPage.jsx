6658ca02cec45eea69b68fc34bfa3d564b9858c6
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-11-04 14:52:16 +0000

    Add NotFoundPage

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+import css from "./NotFoundPage.module.css";
+
+export default function NotFoundPage() {
+  return <div className={css.container}>
+    <p className={css.text}>Sorry, page not found</p>
+    </div>;
+}
